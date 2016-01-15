09bebd71838f266a787cdddb5fd4046ce1206c52
michaeldada <mail.michaeldada@gmail.com>
2016-01-15 18:47:43 +0000

    add basic branching functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+    $("#userInput").submit(function(event) {
+    $("#mex, #sf, #france").hide();
+
+    var season = $("input:radio[name=season]:checked" ).val();
+
+    if (season === 'summer') {
+      $("#mex").show();
+    } else if (season === 'winter') {
+      $('#sf').show();
     
+  };
+    event.preventDefault();
+  });
+
 });
